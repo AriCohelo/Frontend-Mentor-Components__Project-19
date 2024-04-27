1a629b3f46e7aac2a35ce19da83fd17362dda501
AriCohelo <ariel.cohelo@gmail.com>
2024-04-27 13:21:51 +0200

    solved the problem with svg <use> going for the JS to change the color

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './main.scss';
+
+let notifyForm = document.forms.notifyForm;
+let emailInput = notifyForm[0];
+let submitButton = notifyForm[1];
+let emailError = document.querySelector('.pingLaunch__form-email-error');
+submitButton.addEventListener('click', (e) => {
+	if (!emailInput.checkValidity()) {
+		e.preventDefault();
+		emailError.style = 'display: block';
+	}
+});
